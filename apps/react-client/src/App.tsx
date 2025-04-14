ea2b2c9cb78815411e23b469ddefd38d3417df9f
harsh-05 <harshverma09022001@gmail.com>
2025-04-14 22:57:23 +0530

    Added  tailwindCSS

---
+++ apps/react-client/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-red-500">
+        TailwindCSS is working fine.
+    </div>
+  )
+}
+
+export default App
--- apps/webSocket-backend/src/index.ts
+++ apps/webSocket-backend/src/index.ts
@@ -6,5 +6,5 @@ import {port} from "@repo/utils"
 const wss = new WebSocketServer({ port: port });
 
 wss.on("connection", (ws) => {
-
+    console.log(wss.clients.entries());
 })
