d5420e56b966b3c20c849bf5d10648fa58b10f4d
harsh-05 <harshverma09022001@gmail.com>
2025-04-17 10:29:29 +0530

    Added responsive canvas according to the window inner height and width, fixed the scrollbar problem

--- apps/react-client/src/App.tsx
+++ apps/react-client/src/App.tsx
@@ -1,20 +1,32 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
 
 
 function App() {
-  
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  }) 
   const canvasref = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const ctx = canvasref.current?.getContext("2d");
-    
+    console.log(dimensions.height, dimensions.width);
+  }, [dimensions])
+
+  useEffect(() => {
+    function resize() {
+        setDimensions(()=>({width: window.innerWidth, height: window.innerHeight}))
+    }
+    resize();
+    window.addEventListener("resize", resize);
+
+    return () => window.addEventListener("resize", resize);
   }, [])
 
 
   return (
-    <canvas ref={canvasref} width={window.innerWidth} height={window.innerHeight}>
-      
-    </canvas>
+    <canvas ref={canvasref} width={dimensions.width} height={dimensions.height} style={{ backgroundColor: 'grey'}}></canvas>
   );
 }
 
