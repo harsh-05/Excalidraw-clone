37538c5fa316396154603473ea15febab462e44b
harsh-05 <harshverma09022001@gmail.com>
2025-04-21 12:33:14 +0530

    Added the class approach and changed the drawing logic

--- apps/react-client/src/App.tsx
+++ apps/react-client/src/App.tsx
@@ -1,85 +1,132 @@
-import { useEffect, useRef, useState } from "react";
-
-
+import { useCallback, useEffect, useRef, useState } from "react";
+import Shape from "./shapes/shape";
+import Rectangle from "./shapes/rectangle";
 
 function App() {
   const [dimensions, setDimensions] = useState({
     width: window.innerWidth,
-    height: window.innerHeight
-  }) 
+    height: window.innerHeight,
+  });
   const canvasref = useRef<HTMLCanvasElement>(null);
 
-  
+  const [shapes, setShapes] = useState<Shape[]>([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [startCoords, setStartCoords] = useState<{
+    startx: number;
+    starty: number;
+  } | null>(null);
+
+  // will hold the one shape, which is currently is drawing.
+  const [previewShape, setPreviewShape] = useState<Shape | null>(null);
+
   // rendering will happen according to the size of the window.. I guess...
-  useEffect(() => {
+
+  // Function to draw & redraw the canvas, based on the technique "clear and draw". This is inefficient in large size canvas.
+  const drawCanvas = useCallback(() => {
     const canvas = canvasref.current;
     const ctx = canvas?.getContext("2d");
-    if (!ctx) {
-      return
-    }
+    if (!ctx || !canvas) return;
 
-    let mousedown = false;
-    let startX = 0;
-    let startY = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const shapes:{type:string, x: number, y:number, width: number, height: number}[] = []
-
-    canvas?.addEventListener("mousedown", (e) => {
-      mousedown = true;
-      startX = e.offsetX;
-      startY = e.offsetY;
+    //Drawing all the existing shapes.
+    shapes?.map((shape) => {
+      shape.draw(ctx);
     });
 
-    canvas?.addEventListener("mousemove", (e) => {
-      if (!mousedown) {
-        return;
-      }
-       let width = e.offsetX - startX;
-       let height = e.offsetY - startY;
-       ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (previewShape) {
+      previewShape.draw(ctx);
+    }
+  }, [shapes, previewShape]);
 
-       //Mapping all the rectangles here.
-      shapes.map((shape) => {
-        
-        return (
-          ctx.strokeRect(shape.x, shape.y, shape.width, shape.height) 
-         
-       );
-      }
-      );
-      
-         
-      
-      ctx.strokeRect(startX, startY, width, height);
-      
-    })
+  function getPosition(e: React.MouseEvent<HTMLCanvasElement>) {
+    const rect = e.currentTarget.getBoundingClientRect(); // Refer Google Docs for better understanding...
+    const x = e.clientX - rect.x;
+    const y = e.clientY - rect.y;
+    return { x, y };
+  }
 
-    canvas?.addEventListener("mouseup", (e) => {
-      mousedown = false;
-       let width = e.offsetX - startX;
-       let height = e.offsetY - startY;
-      shapes.push({ type: "rect", x: startX, y: startY, width, height });
-    })
-  
+  //Functions to handle the mousedown, mousemove, mouseup
+  function handleMouseDown(e: React.MouseEvent<HTMLCanvasElement>) {
+    setIsDrawing(true);
+    //Giving start coordinates currently to clientx and clientY, find why offsetX and offsetY is not working here.
+    // It is better to get the position this, find the reason and note it down in the google docs.
+    const { x, y } = getPosition(e);
+    setStartCoords({ startx: x, starty: y });
+    setPreviewShape(null);
+  }
 
-  }, [dimensions])
+  function handleMouseMove(e: React.MouseEvent<HTMLCanvasElement>) {
+    if (!canvasref.current || isDrawing === false) return;
+    if (!startCoords) return;
 
-  
+    const currentPos = getPosition(e);
+    const width =  currentPos.x - startCoords?.startx ;
+    const height = currentPos.y - startCoords?.starty  ;
+
+   
+    const rect = new Rectangle(
+      startCoords?.startx,
+      startCoords?.starty,
+      width,
+      height
+    );
+
+    setPreviewShape(rect);
+  }
+
+  function handleMouseup(e: React.MouseEvent<HTMLCanvasElement>) {
+    if (!canvasref.current || isDrawing === false) return;
+    if (!startCoords) return;
+
+    const currentPos = getPosition(e);
+    const width = currentPos.x - startCoords?.startx;
+    const height = currentPos.y - startCoords?.starty;
+    const rect = new Rectangle(
+      startCoords?.startx,
+      startCoords?.starty,
+      width,
+      height
+    );
+
+    setShapes((prevshapes) => [...prevshapes, rect]);
+
+    setIsDrawing(false);
+    setPreviewShape(null);
+    setStartCoords(null);
+  }
+
+  // Drawing and redrawing canvas on every shape change or previewShape change.
+  useEffect(() => {
+    drawCanvas();
+  }, [drawCanvas]);
+
   // Upon loading of the page then giving the size of the canvas accroding to the window's dimensions, added a event listener to make it responsive.
   useEffect(() => {
+    if (!canvasref.current) return;
+
     function resize() {
-        setDimensions(()=>({width: window.innerWidth, height: window.innerHeight}))
+      setDimensions(() => ({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }));
     }
-    resize();
+
     window.addEventListener("resize", resize);
 
     return () => window.removeEventListener("resize", resize);
-  }, [])
-
+  }, [dimensions]);
 
   return (
-    <canvas ref={canvasref} width={dimensions.width} height={dimensions.height} ></canvas>
+    <canvas
+      ref={canvasref}
+      width={dimensions.width}
+      height={dimensions.height}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseup}
+      onMouseMove={handleMouseMove}
+    ></canvas>
   );
 }
 
-export default App
+export default App;
--- apps/react-client/src/shapes/rectangle.ts
+++ apps/react-client/src/shapes/rectangle.ts
@@ -3,12 +3,14 @@ import Shape from "./shape";
 class Rectangle extends Shape {
     
 
-    constructor(x: number, y: number, width: number, height: number, type: "Rectangle" | "Circle" | "Line") {
-        super(x,y,width, height,type)
+
+    // We'll use class to hold the properties such as fillstyle, strokestyle, linewidth, etc.......
+    constructor(x: number, y: number, width: number, height: number) {
+        super(x,y,width, height, "Rectangle")
     }
 
     draw(context: CanvasRenderingContext2D): void {
-        
+        context.strokeRect(this.x, this.y, this.width, this.height);
     }
 }
 
